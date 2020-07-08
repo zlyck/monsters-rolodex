27d9abb60571bb98de3bab09b79e712bb603a051
Zlyck <zlyck@iMac.local>
2020-07-08 18:26:56 +0700

    Updates

---
+++ precache-manifest.f75d2fbf85269bb9bd6448a9f8fea8f9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "40bbed7fa06be75516abab0f919553ac",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "124a099ff3e1a05fba69",
+    "url": "/monsters-rolodex/static/css/main.e9827be8.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "124a099ff3e1a05fba69",
+    "url": "/monsters-rolodex/static/js/main.09d5b38c.chunk.js"
+  },
+  {
+    "revision": "f051cecc4ee3cb3b301f",
+    "url": "/monsters-rolodex/static/js/runtime-main.eea62ddf.js"
+  }
+]);
